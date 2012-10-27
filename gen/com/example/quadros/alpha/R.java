ee921fe76d7c1ab7be474153ed21525c92d19c3f
Viet Le <viet@CJ9.(none)>
2012-10-26 22:22:37 -0500

    working pages

--- gen/com/example/quadros/alpha/R.java
+++ gen/com/example/quadros/alpha/R.java
@@ -11,14 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int heart=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int title_icon=0x7f020003;
     }
     public static final class id {
-        public static final int menuAction_button=0x7f070003;
-        public static final int menu_view_textViewId=0x7f070000;
-        public static final int playAction_button=0x7f070001;
-        public static final int title_view_textViewId=0x7f070002;
+        public static final int correctAction_button=0x7f07000b;
+        public static final int difficultyAction_button=0x7f070005;
+        public static final int heart1=0x7f070006;
+        public static final int heart2=0x7f070007;
+        public static final int heart3=0x7f070008;
+        public static final int heart4=0x7f070009;
+        public static final int incorrectAction_button=0x7f07000c;
+        public static final int menuAction_button=0x7f07000e;
+        public static final int menu_view_textViewId=0x7f070001;
+        public static final int optionsAction_button=0x7f070004;
+        public static final int playAction_button=0x7f070002;
+        public static final int play_view_textViewId=0x7f07000a;
+        public static final int progressAction_button=0x7f070003;
+        public static final int title_icon=0x7f070000;
+        public static final int title_view_textViewId=0x7f07000d;
     }
     public static final class layout {
         public static final int menu_view=0x7f030000;
